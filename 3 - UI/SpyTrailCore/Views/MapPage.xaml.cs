def6464f77185ea1568626626f2e0ebd8dd80365
oscar.Jaergren <oscar.Jaergren@gmail.com>
2019-09-17 20:54:48 +0100

    Map now loads properly and gets location from user

--- 3 - UI/SpyTrailCore/Views/MapPage.xaml.cs
+++ 3 - UI/SpyTrailCore/Views/MapPage.xaml.cs
@@ -1,14 +1,61 @@
 ﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.Maps;
 
 namespace SpyTrail.Views
 {
+    using System;
+    using System.Threading.Tasks;
+
+    using Xamarin.Essentials;
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            await this.GetLocation();
+        }
+
+        private async Task GetLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                    this.MoveMap(location);
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        private void MoveMap(Location location)
+        {
+            this.MyMap.MoveToRegion(new MapSpan(new Position(location.Latitude, location.Longitude), 180, 180));
         }
     }
 }
